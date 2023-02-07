e7094db79ee804e13b36a091e056e0e17312420d
Sujeet Krithik <sujeetkrithik@gmail.com>
2023-02-07 22:52:57 +0530

    adding files

---
+++ src/main/java/com/example/RoleBasedLogin.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedLogin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedLogin.class, args);
+	}
+
+
+}
